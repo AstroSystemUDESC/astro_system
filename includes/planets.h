302c184b212abcd3c77e3e50adecef883099df29
isabellirosa <isabelli.ifc@gmail.com>
2026-07-06 21:55:21 -0300

    Feat: Formulas e informaçoes sobre os planetas(parte Isabelli)

---
+++ includes/planets.h
@@ -0,0 +1,11 @@
+#ifndef ASTRO_SYSTEM_PLANETS_H
+#define ASTRO_SYSTEM_PLANETS_H
+
+#include <string>
+using namespace std;
+
+void imprimirPlaneta(string nome, string temp, string grav, string massa, 
+                     string dist, string dia, string ano, int luas, 
+                     string comp, string estacao, string visivel, double fatorFuso);
+
+#endif
